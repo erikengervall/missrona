9a132e8e38deab409d129fbda82acfb1f89a4d2f
Erik Engervall <erik.engervall@gmail.com>
2020-04-04 12:30:46 +0200

    add google-maps dep

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
+import Map from "./Map"
+import React from "react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Is there some type of webhook for `master` now or what 👀</p>
-      </header>
+      <Map />
     </div>
   )
 }
---
+++ src/Map.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import { GoogleMap, LoadScript, HeatmapLayer } from "@react-google-maps/api"
+import React from "react"
+
+const center = {
+  lat: 37.774546,
+  lng: -122.433523,
+}
+
+const onClick = (...args) => {
+  console.log("onClick args: ", args)
+}
+
+const ExampleHeatmap = () => (
+  <div id="map">
+    <LoadScript
+      id="script-loader"
+      googleMapsApiKey={"AIzaSyC5cfYaVv1ODiuEuLbdm_EqVLwoXohYbTg"} // https://console.cloud.google.com/apis/credentials
+    >
+      <GoogleMap
+        id="heatmap-example"
+        mapContainerStyle={{
+          position: "static",
+          overflow: "inherit",
+          height: "100vh",
+          width: "100vw",
+        }}
+        zoom={13}
+        center={center}
+        onClick={onClick}
+      >
+        <HeatmapLayer
+          data={[
+            new google.maps.LatLng(37.782, -122.447),
+            new google.maps.LatLng(37.782, -122.445),
+            new google.maps.LatLng(37.782, -122.443),
+            new google.maps.LatLng(37.782, -122.441),
+            new google.maps.LatLng(37.782, -122.439),
+            new google.maps.LatLng(37.782, -122.437),
+            new google.maps.LatLng(37.782, -122.435),
+            new google.maps.LatLng(37.785, -122.447),
+            new google.maps.LatLng(37.785, -122.445),
+            new google.maps.LatLng(37.785, -122.443),
+            new google.maps.LatLng(37.785, -122.441),
+            new google.maps.LatLng(37.785, -122.439),
+            new google.maps.LatLng(37.785, -122.437),
+            new google.maps.LatLng(37.785, -122.435),
+          ]}
+        />
+      </GoogleMap>
+    </LoadScript>
+  </div>
+)
+
+export default ExampleHeatmap
